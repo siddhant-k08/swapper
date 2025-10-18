630acc4163371be61576eb65c450c8e29671626f
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-10-19 00:53:04 +0530

    init

---
+++ contracts/Swap.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/// @notice Uniswap v3 interface
+interface ISwapRouterV3 {
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint256 sqrtPriceLimitX96;
+    }
+
+    function exactInputSingle(
+        ExactInputSingleParams calldata params
+    ) external payable returns (uint256 amountOut);
+}
